6d2a390c5a97699ebb2f492aa0544d8193464845
Tom <cornelius.lundi@gmail.com>
2025-05-31 22:40:08 +0200

    Fixed fonts

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
           <link
-          href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap"
           rel="stylesheet"
         />
         </Head>
